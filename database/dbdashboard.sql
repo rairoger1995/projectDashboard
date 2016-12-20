f22fd9319b79e187c43c0c3b0f9706bb35339dd7
Raimehn Roger <raimehn07@gmail.com>
2016-12-20 14:17:25 +0800

    change the db on files

--- database/dbdashboard.sql
+++ database/dbdashboard.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
+-- version 4.3.11
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Oct 13, 2016 at 02:48 PM
--- Server version: 10.0.27-MariaDB-0ubuntu0.16.04.1
--- PHP Version: 7.0.8-0ubuntu0.16.04.3
+-- Host: 127.0.0.1
+-- Generation Time: Dec 20, 2016 at 07:16 AM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,30 +14,11 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `dbportal`
+-- Database: `dbdashboard`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `gender`
---
-
-CREATE TABLE `gender` (
-  `id` tinyint(4) NOT NULL,
-  `label` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `gender`
---
-
-INSERT INTO `gender` (`id`, `label`) VALUES
-(1, 'Male'),
-(2, 'Female');
 
 -- --------------------------------------------------------
 
@@ -45,7 +26,7 @@ INSERT INTO `gender` (`id`, `label`) VALUES
 -- Table structure for table `status`
 --
 
-CREATE TABLE `status` (
+CREATE TABLE IF NOT EXISTS `status` (
   `id` tinyint(4) NOT NULL,
   `label` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -61,26 +42,41 @@ INSERT INTO `status` (`id`, `label`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_uploads`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_uploads` (
+  `id` int(11) NOT NULL,
+  `filename` varchar(70) NOT NULL,
+  `file_type` varchar(10) NOT NULL,
+  `date_uploaded` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_users`
 --
 
-CREATE TABLE `tbl_users` (
+CREATE TABLE IF NOT EXISTS `tbl_users` (
   `id` int(11) NOT NULL,
   `first_name` varchar(50) NOT NULL,
   `middle_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `user_name` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(70) NOT NULL,
+  `salt` int(20) NOT NULL,
   `user_level` smallint(8) NOT NULL DEFAULT '1' COMMENT '''superuser = 1'', ''admin = 2'', ''user = 3''',
   `status` tinyint(4) NOT NULL COMMENT '''a = 1'',''i =2'' '
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tbl_users`
 --
 
-INSERT INTO `tbl_users` (`id`, `first_name`, `middle_name`, `last_name`, `user_name`, `password`, `user_level`, `status`) VALUES
-(1, 'Raimehn Aldous', 'Dizon', 'Roger', 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, 1);
+INSERT INTO `tbl_users` (`id`, `first_name`, `middle_name`, `last_name`, `user_name`, `password`, `salt`, `user_level`, `status`) VALUES
+(1, 'Raimehn Aldous', 'Dizon', 'Roger', 'admin', '$2y$10$p.xKW7IagCX8c2iu4Ca.zOp.shMaZObGLDSZWeuqiigSEVzoWTCIq', 123456, 1, 1),
+(2, 'user 1', 'user', 'user 1', 'user', '$2y$10$CXH5OkIAB6Wtky.pVR4rleLdWggCIx980UUObeKokuqN6qrF1dy0K', 465355, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -88,16 +84,16 @@ INSERT INTO `tbl_users` (`id`, `first_name`, `middle_name`, `last_name`, `user_n
 -- Table structure for table `user_level`
 --
 
-CREATE TABLE `user_level` (
+CREATE TABLE IF NOT EXISTS `user_level` (
   `id` int(11) NOT NULL,
-  `level` varchar(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `label` varchar(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `user_level`
 --
 
-INSERT INTO `user_level` (`id`, `level`) VALUES
+INSERT INTO `user_level` (`id`, `label`) VALUES
 (1, 'super admin'),
 (2, 'admin'),
 (3, 'user');
@@ -107,15 +103,15 @@ INSERT INTO `user_level` (`id`, `level`) VALUES
 --
 
 --
--- Indexes for table `gender`
---
-ALTER TABLE `gender`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `status`
 --
 ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_uploads`
+--
+ALTER TABLE `tbl_uploads`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -135,15 +131,20 @@ ALTER TABLE `user_level`
 --
 
 --
+-- AUTO_INCREMENT for table `tbl_uploads`
+--
+ALTER TABLE `tbl_uploads`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `user_level`
 --
 ALTER TABLE `user_level`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
